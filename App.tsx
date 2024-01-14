28cf0de89a4172111bd46b897a53af7774fb5b40
wiktorrudzki <wiktor1rudzki@gmail.com>
2024-01-14 13:19:52 +0100

    install dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from 'style';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 };
 
